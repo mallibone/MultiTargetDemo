6a8a8e99d9b80d8ceededd1a5e0e4a8a5aa9a12b
Mark Allibone <mallibone@outlook.com>
2018-05-11 21:12:41 +0200

    Add basic implementation for ios

---
+++ MultiTargetDemo/MultiTargetDemo/Platforms/xamarin.ios10/Greeting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MultiTargetDemo.Platforms
+{
+    class Greeting
+    {
+        public static string Hello() => "Hello from NETStandard 2.0";
+    }
+}
